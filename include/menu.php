466ce304ceb13c245b4e4a86feb3d6775c0d14e5
Shahrukh Niazi <shahrukh.niazi@callcenter.com>
2023-09-13 22:16:58 +0500

    codestructure

---
+++ include/menu.php
@@ -0,0 +1,46 @@
+<div class="main-navigate">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-sm-8 col-12">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <div class="mx-auto">
+                        <a href="#" class="navbar-brand">
+                            <img src="images/logo.png" alt="">
+                        </a>
+                    </div>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="index.php">Home </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="aboutus.php">About us</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="services.php">Retail Store</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Restaurant </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="contactus.php">contact us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+            <div class="col-lg-2 col-sm-12">
+               <div class="buyopt">
+                  <ul>
+                    <li><a href="#"><i class="fas fa-search"></i></a></li>
+                    <li><a href="#"><i class="fas fa-user"></i></a></li>
+                    <li><a href="#"><i class="fas fa-cart-plus"></i></a></li>
+                  </ul>
+               </div>
+            </div>
+        </div>
+     </div>
+</div>
